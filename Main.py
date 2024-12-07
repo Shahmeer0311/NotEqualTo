9591eb100d1843ecfef8191d1b966153e99c675d
Shahmeer0311 <22alis@students.watfordboys.org>
2024-12-07 14:33:14 +0000

    NotEqualTO

---
+++ Main.py
@@ -0,0 +1,19 @@
+a = 71
+b = 29
+c = 29
+print(a != b)
+print(c != b)
+a = "chair"
+b = "table"
+if a != b:
+    print("both are different")
+a =  4 
+b = 5
+if (a == 1) != (b==5):
+    print("Hello")
+oddEven = int(input("Enter a number"))
+if (oddEven %2 != 0):
+    print("Its an odd number")
+else:
+    print("Its an Even number")
+    
